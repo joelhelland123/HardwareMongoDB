b854edaec836be57389cdb9a7e03f51e70ffef4e
joelhelland123 <joel.helland@icloud.com>
2023-01-12 10:16:37 +0100

    Add project files.

---
+++ HardwareStock/Program.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.VisualBasic;
+using MongoDataAccess.DataAccess;
+using MongoDataAccess.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Diagnostics.Metrics;
+
+while (true)
+{
+    HardWareDataAccess db = new HardWareDataAccess();
+    int userChoice = db.InputMenuControl();
+
+    //await db.CreateHardWare(new HardWareModel()
+    //{
+    //    Namn = "Såg",
+    //    Beskrivning = "Kan såga"
+    //});
+
+    //await db.CreateHardWare(new HardWareModel()
+    //{
+    //    Namn = "Hammare",
+    //    Beskrivning = "Kan Hamra"
+
+    //});
+
+
+    switch (userChoice)
+    {
+        case 1:
+            string name = "";
+            string description = "";
+            
+            while (string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("Input name");
+                name = Console.ReadLine();
+            }
+            while (string.IsNullOrEmpty(description))
+            {
+                Console.WriteLine("Input discription");
+                description = Console.ReadLine();
+            }
+
+            var hardWare = new HardWareModel { Name = name, Description = description };
+            await db.CreateHardWare(hardWare);
+            Console.WriteLine("Tool added\n");
+            break;
+        case 2:
+            int counter = 1;
+            var hardWareCollection = await db.GetAllHardWares();
+            foreach (var hardWares in hardWareCollection)
+            {
+                Console.WriteLine($"{counter}. Name:{hardWares.Name}\n" +
+                                  $"   discription: {hardWares.Description}\n");
+                counter++;
+            }
+            Console.WriteLine("\n");
+
+            break;
+
+
+        case 3:
+            await db.HardWareUpdateControl();
+            //var hardwares = await db.GetAllHardWares();
+            //int räknare = 1;
+            //foreach(var hardware in hardwares)
+            //{
+            //    Console.WriteLine(räknare + hardware.Namn);
+            //    räknare++;
+            //}
+            //Console.WriteLine("Välj ett av verktygen");
+            //int val = int.Parse(Console.ReadLine());
+            //HardWareModel valet = hardwares[val-1];
+            //Console.WriteLine("Skriv in ett namn");
+            //string namnet = Console.ReadLine();
+            //valet.Namn = namnet;
+            //var hardwares = new HardWareModel();
+            //await db.UpdateHardWare(db.HardWareControl(hardwares());
+            //var filter = Builders<BsonDocument>.Filter.Eq("Namn", "Hammare");
+            //var update = Builders<BsonDocument>.Update.Set("Namn", filter);
+            //hardWareCollection = await db.GetAllHardWares();
+
+            break;
+        case 4:
+            await db.HardWareDeleteControl();
+            //int counter1 = 1;
+            //int val;
+            //var hardWareCollection1 = await db.GetAllHardWares();
+            //foreach (var hardWares in hardWareCollection1)
+            //{
+            //    Console.WriteLine($"{counter1}. Namn:{hardWares.Namn}\n Beskrivning: {hardWares.Beskrivning}");
+            //    counter1++;
+            //}
+            //Console.WriteLine("Skriv den in siffran på verktyget du önskar");
+            ////val > HardWareCollection.Count() ||
+            //while (!Int32.TryParse(Console.ReadLine(), out val) ||  val < 1)
+            //{
+            //    Console.WriteLine("Du behöver skriva en siffra, välj något av alternativen");
+            //}
+            //await db.DeleteHardWare(hardWareCollection1[val-1]);
+            break;
+        case 5:
+            Environment.Exit(1);
+            break;
+        default:
+            break;
+    }
+
+}
+
+
+
+
---
+++ MongoDataAccess/DataAccess/HardWareDataAccess.cs
@@ -0,0 +1,134 @@
+﻿using MongoDataAccess.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Runtime.CompilerServices;
+
+namespace MongoDataAccess.DataAccess
+{
+    public class HardWareDataAccess
+    {
+        public const string ConnectionString = "mongodb+srv://joel123:helland123@cluster0.ym1o2td.mongodb.net/test";
+        public const string Database = "HardWareDatabase";
+        public const string HardWareCollection = "HardWareCollection";
+
+
+        private IMongoCollection<T> ConnectToMongo<T>(string collection)
+        {
+            var client = new MongoClient(ConnectionString);
+            var db = client.GetDatabase(Database);
+            return db.GetCollection<T>(collection);
+        }
+
+        public async Task<List<HardWareModel>> GetAllHardWares()
+        {
+            var hardwareCollection = ConnectToMongo<HardWareModel>(HardWareCollection);
+            var results = await hardwareCollection.FindAsync(_ => true);
+            return results.ToList();
+        }
+
+        public Task CreateHardWare(HardWareModel hardWare)
+        {
+            var hardWareCollection = ConnectToMongo<HardWareModel>(HardWareCollection);
+            return hardWareCollection.InsertOneAsync(hardWare);
+        }
+
+        public Task UpdateHardWare(HardWareModel hardWare)
+        {
+            var hardWareCollection = ConnectToMongo<HardWareModel>(HardWareCollection);
+            var filter = Builders<HardWareModel>.Filter.Eq("Id", hardWare.Id);
+            return hardWareCollection.ReplaceOneAsync(filter, hardWare, new ReplaceOptions { IsUpsert = true });
+        }
+
+        public Task DeleteHardWare(HardWareModel hardware)
+        {
+            var hardWareCollection = ConnectToMongo<HardWareModel>(HardWareCollection);
+            return hardWareCollection.DeleteOneAsync(h => h.Id == hardware.Id);
+        }
+        public int InputMenuControl()
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine("=======================================");
+            Console.WriteLine("Choose one of ther alternatives");
+            Console.WriteLine("1. Add a new tool");
+            Console.WriteLine("2. Verify what tools are available");
+            Console.WriteLine("3. Update toolinformation");
+            Console.WriteLine("4. Remove a tool");
+            Console.WriteLine("5. End program");
+
+            int val = 0;
+
+            while (!Int32.TryParse(Console.ReadLine(), out val) || val < 1 || val > 5)
+            {
+                Console.WriteLine("Input is invalid, try again");
+            }
+            return val;
+        }
+        public int InputModelControl(int valet)
+        {
+
+
+            int val = 0;
+            while (!Int32.TryParse(Console.ReadLine(), out val) || val > HardWareCollection.Count() || val < 1)
+            {
+                Console.WriteLine("Input is invalid, try again");
+            }
+            return val;
+        }
+        public async Task HardWareUpdateControl()
+        {
+            string NamnÄndring = "";
+            string BeskrivningÄndring = "";
+            int val = 0;
+            int counter = 1;
+            var hardwares = await GetAllHardWares();
+            foreach (var hardWare in hardwares)
+            {
+                Console.WriteLine(counter + " " + hardWare.Name);
+                counter++;
+            }
+
+            Console.WriteLine("Input tool number");
+
+            while (!Int32.TryParse(Console.ReadLine(), out val) || val > HardWareCollection.Count() || val < 1)
+            {
+                Console.WriteLine("Input is invalid, try again");
+            }
+
+            while (string.IsNullOrEmpty(NamnÄndring))
+            {
+                Console.WriteLine("Write a name?");
+                NamnÄndring = Console.ReadLine();
+                hardwares[val - 1].Name = NamnÄndring;
+            }
+            while (string.IsNullOrEmpty(BeskrivningÄndring))
+            {
+                Console.WriteLine("Write a discription?");
+                BeskrivningÄndring = Console.ReadLine();
+                hardwares[val - 1].Description = BeskrivningÄndring;
+            }
+            await UpdateHardWare(hardwares[val - 1]);
+            Console.WriteLine($"Tool updated\n");
+        }
+        public async Task HardWareDeleteControl()
+        {
+            int counter1 = 1;
+            int val;
+            var hardWareCollection1 = await GetAllHardWares();
+            foreach (var hardWares in hardWareCollection1)
+            {
+                Console.WriteLine($"{counter1}. Name:{hardWares.Name}\n" +
+                                  $"   Discpription: {hardWares.Description}\n");
+                counter1++;
+            }
+            Console.WriteLine("Input the number of what tool would you like to remove");
+            //val > HardWareCollection.Count() ||
+            while (!Int32.TryParse(Console.ReadLine(), out val) || val < 1)
+            {
+                Console.WriteLine("Input is invalid, try again");
+            }
+            
+            await DeleteHardWare(hardWareCollection1[val - 1]);
+            Console.WriteLine($"Tool Erased\n");
+        }
+    }
+}
